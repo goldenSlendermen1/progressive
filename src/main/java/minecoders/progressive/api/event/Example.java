f0df99cfcedb5d26c7a836d24c65fb94cac48f3c
= <=>
2026-08-22 21:05:35 -0500

    asdfasdf

--- src/main/java/minecoders/progressive/api/event/Example.java
+++ src/main/java/minecoders/progressive/api/event/Example.java
@@ -3,17 +3,11 @@ package minecoders.progressive.api.event;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 
-/**
- *
- */
 public class Example {
     public interface ExampleInterface {
         void callback();
     }
 
-    /**
-     *
-     */
     public static final Event<ExampleInterface> EXAMPLE = EventFactory.createArrayBacked(ExampleInterface.class, listeners -> () -> {
 
     });
